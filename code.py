09f1dea22d6aaffca8b462af57ddf984592e6b51
unknown <r.gokulrk9@gmail.com>
2019-06-30 12:12:44 -0400

    error correction

--- code.py
+++ code.py
@@ -1,8 +1,7 @@
-a=input("Enter 'a' value: ")
-b=input("Enter 'b' value: ")
+a=input ("Enter 'a' value: ")
+b=input ("Enter 'b' value: ")
 if(b==0):
     print("Divide by zero error")
-    return True 
 else:
-    c=a/b
+    c = int(a) / int(b)
     print(c)
